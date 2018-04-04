073a32d3f8d1ca247433b852b912144a942eecab
karthy07 <mail2karthyk@gmail.com>
2018-04-05 03:29:23 +0530

    add default constructor in connect service

--- GrpcServer/src/main/java/com/github/karthyks/project/era/grpc/server/services/ConnectImpl.java
+++ GrpcServer/src/main/java/com/github/karthyks/project/era/grpc/server/services/ConnectImpl.java
@@ -7,10 +7,14 @@ import io.grpc.stub.StreamObserver;
 
 public class ConnectImpl extends ConnectGrpc.ConnectImplBase {
 
+    public ConnectImpl() {
+        System.out.println("Connect service implemented!");
+    }
+
     @Override
     public void initiate(ConnectRequest request, StreamObserver<ConnectResponse> responseObserver) {
-       ConnectResponse response = ConnectResponse.newBuilder().setStatus("OK").build();
-       responseObserver.onNext(response);
-       responseObserver.onCompleted();
+        ConnectResponse response = ConnectResponse.newBuilder().setStatus("OK").build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
