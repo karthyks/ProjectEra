61b8ba3a20a42bb5fb59831588f879544f960ba1
karthik-logs <karthik.periasami@mtap.in>
2018-04-05 20:42:31 +0530

    add hook service

--- GrpcServer/src/main/java/com/github/karthyks/project/era/grpc/server/ServerPool.java
+++ GrpcServer/src/main/java/com/github/karthyks/project/era/grpc/server/ServerPool.java
@@ -1,6 +1,6 @@
 package com.github.karthyks.project.era.grpc.server;
 
-import com.github.karthyks.project.era.grpc.server.services.ConnectImpl;
+import com.github.karthyks.project.era.grpc.server.services.HookService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
@@ -8,46 +8,48 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 public class ServerPool {
-    private static final Logger logger = Logger.getLogger(ServerPool.class.getName());
-    private Server server;
+  private static final Logger logger = Logger.getLogger(ServerPool.class.getName());
+  private Server server;
 
-    private void start() throws IOException {
-        /* The port on which the server should run */
-        int port = 50051;
-        server = ServerBuilder.forPort(port)
-                .addService(new ConnectImpl())
-                .build()
-                .start();
-        logger.info("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-            System.err.println("*** shutting down gRPC server since JVM is shutting down");
-            ServerPool.this.stop();
-            System.err.println("*** server shut down");
-        }));
-    }
+  private void start(int port) throws IOException {
+    server = ServerBuilder.forPort(port)
+        .addService(new HookService())
+        .build()
+        .start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+      System.err.println("*** shutting down gRPC server since JVM is shutting down");
+      ServerPool.this.stop();
+      System.err.println("*** server shut down");
+    }));
+  }
 
-    private void stop() {
-        if (server != null) {
-            server.shutdown();
-        }
+  private void stop() {
+    if (server != null) {
+      server.shutdown();
     }
+  }
 
-    /**
-     * Await termination on the main thread since the grpc library uses daemon threads.
-     */
-    private void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-            server.awaitTermination();
-        }
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
     }
+  }
 
-    /**
-     * Main launches the server from the command line.
-     */
-    public static void main(String[] args) throws IOException, InterruptedException {
-        final ServerPool server = new ServerPool();
-        server.start();
-        server.blockUntilShutdown();
+  /**
+   * Main launches the server from the command line.
+   */
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final ServerPool server = new ServerPool();
+    if (args == null || args.length <= 0) {
+      server.start(50051);
+    } else {
+      server.start(Integer.parseInt(args[0]));
     }
+    server.blockUntilShutdown();
+  }
 }
---
+++ GrpcServer/src/main/java/com/github/karthyks/project/era/grpc/server/services/HookService.java
@@ -0,0 +1,41 @@
+package com.github.karthyks.project.era.grpc.server.services;
+
+import com.gihub.karthyks.project.era.proto.hook.HookGrpc;
+import com.gihub.karthyks.project.era.proto.hook.HookRequest;
+import com.gihub.karthyks.project.era.proto.hook.HookResponse;
+import io.grpc.stub.StreamObserver;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HookService extends HookGrpc.HookImplBase {
+  private static final List<StreamObserver> observers = new LinkedList<>();
+
+  public HookService() {
+    System.out.println("Ready to be hooked!");
+  }
+
+  @Override
+  public StreamObserver<HookRequest> initiate(final StreamObserver<HookResponse> responseObserver) {
+    return new StreamObserver<HookRequest>() {
+      @Override
+      public void onNext(HookRequest value) {
+        if (!observers.contains(responseObserver))
+          observers.add(responseObserver);
+        System.out.println("Request value " + value.getName());
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        observers.remove(responseObserver);
+      }
+
+      @Override
+      public void onCompleted() {
+        HookResponse hookResponse = HookResponse.newBuilder().setStatus("Hooked").build();
+        responseObserver.onNext(hookResponse);
+        observers.remove(responseObserver);
+      }
+    };
+  }
+}
